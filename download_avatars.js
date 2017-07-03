2933b0b1cffe675a5022a3c09b4cc2c6ac228362
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-07-03 21:51:11 +0000

    created requestURL to link to API

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,17 @@
 const request = require('request');
+const fs = require('fs');
+const GITHUB_USER = "MathewKostiuk";
+const GITHUB_TOKEN = "35bb07448bf255c925e55539ac748f31a799226e";
 
 console.log('Welcome to the Github Avatar Downloaded!');
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  const requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
