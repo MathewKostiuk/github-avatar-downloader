b9d6cd1a77fd961dc3f97bb10aabcef55dcb2ccb
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-07-03 23:49:23 +0000

    completed get request

--- download_avatars.js
+++ download_avatars.js
@@ -1,15 +1,37 @@
 const request = require('request');
 const fs = require('fs');
 const GITHUB_USER = "MathewKostiuk";
-const GITHUB_TOKEN = "35bb07448bf255c925e55539ac748f31a799226e";
+
+
 
 console.log('Welcome to the Github Avatar Downloaded!');
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  const requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  const requestURL = 'https://' + GITHUB_USER + ':' + process.env.GIT_HUB_ACCESS_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   console.log(requestURL);
+
+  const options = {
+    'url': requestURL,
+    'headers': {
+      'User-Agent': 'avatar_downloader'
+    }
+  };
+
+  request.get(options, requestURL)
+  .on('error', function (err) {
+    console.log(err);
+  })
+  .on('response', function (response) {
+    console.log(response.statusCode);
+    //console.log(response.headers);
+    console.log(response);
+  })
 }
+
+
+
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
